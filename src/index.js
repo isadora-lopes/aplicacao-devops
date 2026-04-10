82f1c1e29a7addc8a5d0092721b62a1356a0310c
HevelynLV <hdefreitas989@gmail.com>
2026-04-09 21:42:51 -0300

    Criando o  GET

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ const app = express()
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000')
 })
+
+const data = {
+  integrantes: [
+    { nome: 'Nome Sobrenome' },
+    { nome: 'Nome Sobrenome' },
+    { nome: 'Nome Sobrenome' }
+  ]
+}
+
+app.get('/', (req, res) => {
+  res.sendFile('views/index.html', { root: __dirname })
+})
+
+app.get('/integrantes', (req, res) => {
+  res.json(data)
+})
+
